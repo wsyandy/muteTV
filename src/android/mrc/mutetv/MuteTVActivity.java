3dca3c0b95f36facd481b3ff62d42047675b841f
Jorge Desirat <jdesirat@gmail.com>
2012-05-01 19:43:41 +0100

    first entry

---
+++ src/android/mrc/mutetv/MuteTVActivity.java
@@ -0,0 +1,13 @@
+package android.mrc.mutetv;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MuteTVActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
